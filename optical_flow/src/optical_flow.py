e2eecf6af88032d42c82e3c7ed0180b5608bd412
Ishan Tamrakar <ishantamrakar24@gmail.com>
2025-06-05 12:17:18 -0400

    LK basic working

--- optical_flow/src/optical_flow.py
+++ optical_flow/src/optical_flow.py
@@ -6,7 +6,7 @@ class OpticalFlowBase:
         raise NotImplementedError("Must implement in subclass")
     
 class LucasKanadeOpticalFlow(OpticalFlowBase):
-    def __init__(self, max_corners=100, quality_level=0.3, min_distance=7, win_size=(15,15)):
+    def __init__(self, max_corners=100, quality_level=0.2, min_distance=7, win_size=(15,15)):
         self.feature_params = dict(maxCorners=max_corners, 
                                    qualityLevel=quality_level, 
                                    minDistance = min_distance,
@@ -17,14 +17,23 @@ class LucasKanadeOpticalFlow(OpticalFlowBase):
         self.prev_pts = None
         
     def compute_flow(self, prev_gray, next_gray):
-        if self.prev_pts is None: 
+        if self.prev_pts is None:
             self.prev_pts = cv2.goodFeaturesToTrack(prev_gray, mask=None, **self.feature_params)
+
+        if self.prev_pts is None or len(self.prev_pts) == 0:
+            return [], []
+
+        next_pts, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, next_gray, self.prev_pts, None, **self.lk_params)
+
+        if next_pts is None or status is None:
+            return [], []
+
+        good_new = next_pts[status.flatten() == 1]
+        good_old = self.prev_pts[status.flatten() == 1]
         
-        next_pts, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, next_gray, self.prev_pts, None, **self.lk_params)   
-        
-        good_new = next_pts[status == 1]
-        good_old = self.prev_pts[status == 1]
-        
-        self.prev_pts = good_new.reshape(-1,1,2)
+        if len(good_new) < 10:
+            self.prev_pts = cv2.goodFeaturesToTrack(next_gray, mask=None, **self.feature_params)
+
+        self.prev_pts = good_new.reshape(-1, 1, 2)
         return good_old, good_new
         
